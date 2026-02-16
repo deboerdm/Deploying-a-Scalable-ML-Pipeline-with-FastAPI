306cb568ce57cd55439865e9a621d8d6058fc2a9
ddeboe4 <ddeboe4@wgu.edu>
2026-02-15 17:24:14 -0700

    created test_ml.py test cases

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,45 @@
 import pytest
+
 # TODO: add necessary import
 
+import numpy as np
+import pandas as pd
+import os
+from ml.model import inference, train_model
+from sklearn.model_selection import train_test_split
+
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_inference_function():
     """
-    # add description for the first test
+    # Testing the inference function
     """
-    # Your code here
-    pass
+    X = np.random.rand(20, 5)
+    y = np.random.randint(5, size=20)
+    model = train_model(X, y)
+    y_preds = inference(model, X)
+    assert y.shape == y_preds.shape
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_model_encoder_saved():
     """
-    # add description for the second test
+    # Testing if model and encoder were saved to correct location
     """
-    # Your code here
-    pass
+    model_path = os.path.join('model','model.pkl')
+    encoder_path = os.path.join('model','encoder.pkl')
+    assert os.path.isfile(model_path)
+    assert os.path.isfile(encoder_path)
+
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_train_test_split_sizes():
     """
-    # add description for the third test
+    # testing datasets for expected size
     """
-    # Your code here
-    pass
+    data_path = os.path.join('data','census.csv')
+    data = pd.read_csv(data_path)
+    train, test = train_test_split(data, test_size=0.2)
+    assert train.shape[0] <= np.ceil(data.shape[0] * 0.8)
+    assert test.shape[0] <= np.ceil(data.shape[0] * 0.2)
+
